acb98fff49b8227d747f24abdfd90fdb16ad711f
ZaphodBeebblebrox <22801430+ZaphodBeebblebrox@users.noreply.github.com>
2025-09-02 12:32:51 -0400

    Add stub livechart info service

---
+++ src/services/info/livechart.py
@@ -0,0 +1,40 @@
+# Dummy info handler, used for livechart link
+
+from logging import debug, info, warning, error
+import re
+
+from .. import AbstractInfoHandler
+
+class InfoHandler(AbstractInfoHandler):
+    _show_link_base = "https://livechart.me/anime/{id}/"
+    _show_link_matcher = "https?://(?:www\.)?livechart\.me/anime/([0-9]+)"
+
+    def __init__(self):
+        super().__init__("livechart", "LiveChart")
+
+    def get_link(self, link):
+        if link is None:
+            return None
+        return self._show_link_base.format(id=link.site_key)
+
+    def extract_show_id(self, url):
+        if url is not None:
+            match = re.search(self._show_link_matcher, url, re.I)
+            if match:
+                return match.group(1)
+        return None
+
+    def find_show(self, show_name, **kwargs):
+        return list()
+
+    def find_show_info(self, show_id, **kwargs):
+        return None
+
+    def get_episode_count(self, link, **kwargs):
+        return None
+
+    def get_show_score(self, show, link, **kwargs):
+        return None
+
+    def get_seasonal_shows(self, year=None, season=None, **kwargs):
+        return list()
